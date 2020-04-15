014db35772ce7c286bdccbee58fc4b6bdf51ba7b
Raman Heera <rheera2@uwo.ca>
2020-04-15 17:23:56 -0400

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Raman/PycharmProjects/CoreySchaferTutorial/DjangoPractice/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
