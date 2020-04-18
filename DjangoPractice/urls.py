655b606c6b84b79e35685c832ce048e67b022ed4
Raman Heera <rheera2@uwo.ca>
2020-04-18 16:27:08 -0400

    Registration form
    - registration form working
    - path set up to /register

--- DjangoPractice/urls.py
+++ DjangoPractice/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.contrib import admin
 # need to add include
 from django.urls import path, include
+# instead of making a urls.py in the app users we can import it directly to the project urls.py
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     # now when we open the browser and go to /blogs we will map to our urls file in blog folder which maps to our home view
     # path('blog/', include("blog.urls")),
     # if we want the homepage to be the blog then we can leave the path empty
     path('', include("blog.urls")),
-
+    # site/register will take them to our app/views and use the register function, named it register
 ]
--- users/views.py
+++ users/views.py
@@ -6,4 +6,4 @@ def register(request):
     # create a new instance of the form
     form = UserCreationForm()
     # always need to pass request, then template name, then context
-    return render(request, 'users/register.html', {'form: form'})
+    return render(request, 'users/register.html', {'form': form})
