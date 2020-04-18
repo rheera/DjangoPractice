6456b608e41d21350dea4c0ffd1aaac4139330e4
Raman Heera <rheera2@uwo.ca>
2020-04-18 17:13:58 -0400

    Registration form checks
    - added some checks to redirect if the user is valid
    - or return the form with just the username if there were errors

--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+# need to import redirect as well since, after successful sign up they should move to another page
+from django.shortcuts import render, redirect
 # django has a form class already
 from django.contrib.auth.forms import UserCreationForm
+# importing flash messages, flash let's use see a one time message that is gone in the next request
+# ex. messages.debug, messages.info, messages.success, messages.warning, messages.error
+from django.contrib import messages
 
 def register(request):
-    # create a new instance of the form
-    form = UserCreationForm()
+    # if we get a post request, we will instantiate a user creation form with the post data
+    # we submit the registration form (it's a post update) --> if it isn't valid it drops down to return back to request page with the post information which is the username
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        # checks if the form is valid when it's submitted
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            # since it is valid we'll send a success message
+            messages.success(request, f"Account created for {username}!")
+            return redirect("blog-home")
+    # otherwise it'll just be an empty form
+    else:
+        # create a new instance of the form
+        form = UserCreationForm()
     # always need to pass request, then template name, then context
     return render(request, 'users/register.html', {'form': form})
