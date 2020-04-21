de069627268a6ab9492877c63d26d10b6316cd2b
Raman Heera <rheera2@uwo.ca>
2020-04-21 10:43:16 -0400

    Resize profile pictures
    - added a method to resize profile pics to a max of  300x300

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 # need to import User since the profile will be a one to one relationship between the two
 from django.contrib.auth.models import User
-
+# need pillow to resize image
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,3 +15,16 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+
+    # method that already exists in parent class, ran after our model is saved
+    # this method might not be the most efficient to resize photos, but it's simple
+    def save(self):
+        # this runs our normal save function
+        super().save()
+        # opens image of the current instance
+        img = Image.open(self.image.path)
+        # if the height or width of the image is greater than 300 resize it to 300 by 300
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
