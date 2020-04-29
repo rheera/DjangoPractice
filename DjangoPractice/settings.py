aee353e5e0b99b9cf547ed66261b07985538a21b
Raman Heera <rheera2@uwo.ca>
2020-04-29 15:16:14 -0400

    Changing debug for production
    Debug = False

--- DjangoPractice/settings.py
+++ DjangoPractice/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'za7kt73*6s%o3sjglolmuno+*+0b50krn7*ly+zb&w^$esit*s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
