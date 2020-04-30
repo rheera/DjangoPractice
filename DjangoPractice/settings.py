b70969b786c296654db26795b46ed7a8df5abc8a
Raman Heera <rheera2@uwo.ca>
2020-04-30 15:47:55 -0400

    Django_Heroku
    - importing local settings to heroku
    - updating requirements.txt
    - installed Postgres for produciton database

--- DjangoPractice/settings.py
+++ DjangoPractice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,3 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+# automatically sets a lot of our configurations for Heroku
+django_heroku.settings(locals())
