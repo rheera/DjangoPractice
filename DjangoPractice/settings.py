4639c691152def84deddf9f8aaee6954f82b44c2
Raman Heera <rheera2@uwo.ca>
2020-05-03 14:56:04 -0400

    Debug = Env Variable

--- DjangoPractice/settings.py
+++ DjangoPractice/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # set debug to environment variable, since environment variable is a string and we need bool do this workaround
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['django-practice-blog.herokuapp.com']
 
